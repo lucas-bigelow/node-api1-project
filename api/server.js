06480a8527737f4f1e9c01eaa83ac7b85add1403
Lucas Bigelow <lukejbigelow@gmail.com>
2022-06-06 18:25:13 -0500

    completed set up and execution of the MVP tasks for the day

--- api/server.js
+++ api/server.js
@@ -1,3 +1,40 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const db = require('./users/model');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+  db.find().then(response => {
+    res.json(response);
+  })
+})
+
+server.post('/api/users', (req, res) => {
+  db.insert(req.body).then(response => {
+    res.status(201).json(response);
+  })
+})
+
+server.get('/api/users/:id', (req, res) => {
+  db.findById(req.params.id).then(response => {
+    console.log(response)
+    res.json(response);
+  })
+})
+
+server.delete('/api/users/:id', (req, res) => {
+  db.remove(req.params.id).then(response => {
+    res.json(response);
+  })
+})
+
+server.put('/api/users/:id', (req, res) => {
+  db.update(req.params.id, req.body).then(response => {
+    res.json(response);
+  })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const server = require('./api/server');
 const port = 9000;
 
 // START YOUR SERVER HERE
+
+server.listen(port, () => 'Server listening on port 9000');
